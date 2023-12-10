7c16d373f0b6f7f8f03d1915372c5790ddb4a34a
tktk0330 <tkmsnd0330@gmail.com>
2023-12-10 20:43:19 +0900

    Initial Commit

---
+++ DOTENKOPlus/DOTENKOPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOTENKOPlusApp.swift
+//  DOTENKOPlus
+//
+//  Created by Takuma Shinoda on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct DOTENKOPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
